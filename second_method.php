ee92e00ab4220a00f0ddec2633f84f8a70524aff
marcotrunc <marcotrunc@gmail.com>
2022-02-28 18:50:18 +0100

    added first and second method

--- second_method.php
+++ second_method.php
@@ -1,16 +1,20 @@
+<!---------------------------------------------------->
+<!--SECONDO METODO-->
+<!---------------------------------------------------->
+
 <?php
 $faq = [
     [
         'request' => 'Come state implementando la recente decisione della Corte di giustizia dell\' Unione europea (CGUE) relativa al diritto all\'oblio?',
         'answer' => '
-                    <p>La recente <a href="#">decisione della Corte di giustizia dell\'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. 
+                    <p>La recente decisione della Corte di giustizia dell\'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. 
                     La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi 
                     a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, 
                     irrilevanti o non più rilevanti, o eccessivi.</p>
                     <p>Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. 
                     Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto 
                     dell\'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.</p>
-                    <p>Per presentare una richiesta di rimozione, compila questo <a href="#">modulo web</a>. Riceverai una risposta automatica che conferma 
+                    <p>Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma 
                     la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po\' di tempo 
                     perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono ù
                     informazioni obsolete relative alla tua vita privata. 
@@ -25,7 +29,7 @@ $faq = [
                     <p>Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
                     modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando 
                     cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.</p>',
-
+        'links' => ['decisione della Corte di giustizia dell\'Unione europea', 'modulo web.']
     ],
     [
         'request' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
@@ -40,10 +44,10 @@ $faq = [
                     la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google,
                     hai il massimo controllo.</p>
                     <p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, 
-                    nel <a href="#">Centro Google per la sicurezza online </a>.</p>
-                    <p><a href="#">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, 
+                    nel Centro Google per la sicurezza online.</p>
+                    <p>Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, 
                     lasciando a te il controllo.</p>',
-
+        'links' => ['Centro Google per la sicurezza online.', 'Scopri']
     ],
     [
         'request' => 'Perché il mio account è associato a un paese?',
@@ -58,7 +62,7 @@ $faq = [
                         <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali. </li>
                     </ol>
                     <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>',
-
+        'links' => ['decisione della Corte di giustizia dell\Unione europea', 'modulo web'],
     ],
     [
         'request' => 'Stabilire il paese associato al tuo account',
@@ -71,99 +75,85 @@ $faq = [
                     <p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, 
                     il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, 
                     l\'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure 
-                    la residenza vicino a un confine territoriale. <a href="#">Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.</p>',
-
+                    la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.</p>',
+        'links' => [],
     ],
     [
         'request' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
         'answer' => '
                     <p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
                     I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca 
-                    da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href="#">contattare il webmaster</a> 
+                    da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster 
                     del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee 
                     per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati 
                     di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, 
                     le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, 
-                    è inoltre possibile <a href="#">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>. </p>',
-
+                    è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni. </p>',
+        'links' => ['contattare il webmaster', 'visitare la nostra pagina di assistenza per avere ulteriori informazioni.', 'fai clic qui.']
     ],
     [
         'request' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
         'answer' => '
                     <p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, 
                     il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, 
-                    o URL, della pagina dei risultati di ricerca sotto forma di <a href="#">URL referrer</a>. 
+                    o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. 
                     Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. 
                     Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), 
                     nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. 
                     Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. 
-                    Ulteriori informazioni sulla ricerca SSL sono disponibili <a href="#">qui</a>. 
+                    Ulteriori informazioni sulla ricerca SSL sono disponibili qui. 
                     Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google 
                     Analytics o un\'API (Application Programming Interface). 
                     Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\' esatte parole chiave che hanno 
                     determinato il clic su un annuncio.</p>',
-
+        'links' => ['URL referrer', 'qui.'],
     ],
 ];
 
-$menu = ['Introduzione', 'Norme sulla privacy', 'Termini di servizio', 'Tecnologie', 'Domande frequenti'];
 ?>
-<!DOCTYPE html>
-<html lang="en">
+< DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Shadows+Into+Light&family=Supermercado+One&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>google faq</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
 
-<body>
-    <!-- Header -->
-    <header>
-        <div id="top-header">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                    <div class="col-1 d-flex align-items-center">
-                        <figure class=" m-0 py-2">
-                            <img src="./img/logo.png" alt="" class="img-fluid">
-                        </figure>
-                    </div>
-                    <div class="col-2">
-                        <h5 class="text-muted">Privacy e termini</h5>
-                    </div>
+    <body>
+        <-- Header -->
+            //Main
+            <main class="container">
+                <div id="main-wrapper" class="py-5 px-3">
+                    <?php foreach ($faq as $item) {
+                        echo '<h2>' . $item['request'] . '</h2>';
+                        // Creazione array dei link
+                        $links = $item['links'];
+
+                        foreach ($links as $string) {
+
+                            // Dichiaro la stringa da cercare e quella da sosituire
+                            $searched_word = $string;
+                            $replaced_string = '<a href="#">' . $searched_word . '</a>';
+
+                            // Vedo se la stringa da sostituire è presente
+                            $pos = strpos($item['answer'], $searched_word);
+
+                            if ($pos) {
+                                $item['answer'] = str_replace(
+                                    $searched_word,
+                                    $replaced_string,
+                                    $item['answer']
+                                );
+                            }
+                        }
+                        echo '<p>' . $item['answer'] . '</p>';
+                    } ?>
                 </div>
-            </div>
-        </div>
-        <nav class="container-fluid">
-            <ul class="ps-0">
-                <?php foreach ($menu as $item) : ?>
-                    <li><a href="" class="text-muted"><?= $item ?></a></li>
-                <?php endforeach ?>
+            </main>
+            <!-- Footer -->
+    </body>
 
-            </ul>
-        </nav>
-    </header>
-    <!-- Main -->
-    <section class="main py-6">
-        <div class="container">
-            <?php foreach ($faq as $item) : ?>
-                <div class="title">
-                    <h2><?= $item['request'] ?></h2>
-                </div>
-                <div class="paragraph">
-                    <p><?= $item['answer'] ?></p>
-                </div>
-
-            <?php endforeach ?>
-        </div>
-    </section>
-    <!-- footer -->
-</body>
-
-</html>
+    </html>
